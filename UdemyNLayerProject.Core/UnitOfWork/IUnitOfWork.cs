0fa0e7b20f7a8d05b74290899246fc5a0f5968b0
Talha Yazar <talhayazarg@gmail.com>
2022-01-04 16:31:19 +0300

    Lesson 5 : Created UnitOfWork Interface

---
+++ UdemyNLayerProject.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository Product { get; }
+        ICategoryRepository Category { get; }
+
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
