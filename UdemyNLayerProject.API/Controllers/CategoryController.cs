9a7f508efb15433cd63bc4bdd2df6ae8a74fefd1
Talha Yazar <talhayazarg@gmail.com>
2022-01-10 15:11:08 +0300

    Lesson 14 : Created first controller and tested connection between tiers Successfully

---
+++ UdemyNLayerProject.API/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyNLayerProject.Core.Services;
+
+namespace UdemyNLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this._categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await this._categoryService.GetAllAsync();
+            return Ok(categories);
+        }
+
+    }
+}
