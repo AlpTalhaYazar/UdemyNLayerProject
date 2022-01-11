9fede1bb1ba55d0a67f00a9621fb86d983e05d4d
Talha Yazar <talhayazarg@gmail.com>
2022-01-12 00:23:00 +0300

    Changed Product's "InnerBarcode" properties entity configuration for nullable

---
+++ UdemyNLayerProject.Data/Migrations/20220111181319_fixfornullable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UdemyNLayerProject.Data.Migrations
+{
+    public partial class fixfornullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InnerBarcode",
+                table: "Products",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldDefaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InnerBarcode",
+                table: "Products",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
