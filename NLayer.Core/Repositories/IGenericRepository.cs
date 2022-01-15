f3ad6ae5fc9575ab040f40ef510750893738fcbd
Talha Yazar <talhayazarg@gmail.com>
2022-01-15 17:38:08 +0300

    Core Layer Implemented
    Models(Entities) created and defined with conventions.
    GenericRepository, Service and UnitOfWork interfaces created and filled with necessary methods.

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<IQueryable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+    }
+}
