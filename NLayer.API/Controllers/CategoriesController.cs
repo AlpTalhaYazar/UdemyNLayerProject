f72156bb39b7139c160ac4554509fe5dc30fcdbc
Talha Yazar <talhayazarg@gmail.com>
2022-01-19 14:26:02 +0300

    Created Controller for Category
    Created new api controller with CRUD and specific ActionResults for category

---
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly ICategoryService<CategoryDto> _categoryService;
+
+        public CategoriesController(IMapper mapper, ICategoryService<CategoryDto> categoryService)
+        {
+            _mapper = mapper;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            return CreateActionResult(await _categoryService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return CreateActionResult(await _categoryService.GetByIdAsync(id));
+        }
+
+        [HttpGet("[action]/{categoryId}")]
+        public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId)
+        {
+            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryDto categoryDto)
+        {
+            return CreateActionResult(await _categoryService.AddAsync(categoryDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CategoryDto categoryDto)
+        {
+            return CreateActionResult(await _categoryService.updateAsync(categoryDto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return CreateActionResult(await _categoryService.RemoveAsync(category.Data));
+        }
+    }
+}
