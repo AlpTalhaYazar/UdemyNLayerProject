8d3cf1eeb099f75649ad593a1a013bd2756f8c5e
Talha Yazar <talhayazarg@gmail.com>
2022-01-21 12:27:36 +0300

    Product Save Action
    Created a new Action for adding new products to system.
    We can successfully add new items to database.

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NLayer.Core.DTOs;
 using NLayer.Core.Services;
 
@@ -7,15 +8,42 @@ namespace NLayer.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService<ProductDto> _productService;
+        private readonly ICategoryService<CategoryDto> _categoryService;
 
-        public ProductsController(IProductService<ProductDto> productService)
+        public ProductsController(IProductService<ProductDto> productService, ICategoryService<CategoryDto> categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _productService.GetAllAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Save()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = await _categoryService.GetAllAsync();
+
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+
+            return View();
         }
     }
 }
